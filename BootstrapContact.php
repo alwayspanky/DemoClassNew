d6240aa440692bc600a173de2e6fddd763d5e34e
pankaj bhisikar <42427701+alwayspanky@users.noreply.github.com>
2021-12-20 21:00:19 +0530

    added php code reusability and row column concept

---
+++ BootstrapContact.php
@@ -0,0 +1,5 @@
+<?php include "header.html" ?>
+       
+<button class="btn btn-danger" > Delete </button>
+
+<?php include "footer.html"?>
---
+++ index.php
@@ -0,0 +1,90 @@
+<?php include "header.html" ?>
+        <!-- Banner Area -->
+        <div>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img style="height: 600px;" src="https://images.pexels.com/photos/1363876/pexels-photo-1363876.jpeg?cs=srgb&dl=pexels-travis-blessing-1363876.jpg&fm=jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img style="height: 600px;" src="https://images.pexels.com/photos/2080960/pexels-photo-2080960.jpeg?cs=srgb&dl=pexels-johannes-plenio-2080960.jpg&fm=jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img style="height: 600px;" src="https://images.unsplash.com/photo-1498263382026-c65d01dad017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+
+        </div>
+
+        <!-- banner are end -->
+
+        <div style="margin-top:20px; justify-content:center" >
+
+          <div class="row">
+            <div class="col-1 ">
+          </div>
+            <div class=" col-sm-12 col-lg-3 " style="background:#FBFAF0;margin:10px;  border-radius:10px; padding:10px">
+              <h3 style="text-align:center; color:#343a40">Web Development</h3>
+              <p>Web development is the work involved in developing a website for the Internet or an intranet.
+                 Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.</p>
+
+            </div>
+            <div class="col-3" style="background:#FBFAF0; margin:10px;   border-radius:10px; padding:10px">
+            <h3 style="text-align:center; color:#343a40">  Mobile Development</h3>
+            <p style="justify-content:right">Mobile app development is the act or process by which a mobile app is developed for mobile devices, such as personal digital assistants, enterprise digital assistants or mobile phones. 
+            These software applications are designed to run on mobile devices, such as a smartphone or tablet computer.</p>
+
+            </div>
+
+            <div class="col-3" style="background:#FBFAF0; margin:10px;  border-radius:10px; padding:10px">
+            <h3 style="text-align:center; color:#343a40">Digital Marketing</h3>
+            <p>Digital marketing, also called online marketing, is the promotion of brands to connect with potential customers using the internet and other forms of digital communication. This includes not only email, social media, and web-based advertising, but also text and multimedia messages as a marketing channel.</p>
+
+            </div>
+
+            
+          </div>
+
+        </div>
+
+        <!-- about content -->
+
+        <div>
+          <div class="row" style="margin:5%">
+            <div class="col">
+              <img src="" />
+            </div>
+          <div class="col">
+            <h2>About Us</h2>
+            <p>Digital marketing, also called online marketing, is the promotion of brands to connect with potential customers using the internet and other forms of digital communication. This includes not only email, social media, and web-based advertising, but also text and multimedia messages as a marketing channel.</p>
+          </div>
+</div>
+</div>
+        
+
+        
+
+        <div style="height: 300px;">
+            
+        </div>
+
+
+        <div style="height: 300px;">
+            
+        </div>
+
+        <div id="contactPage" style="height: 300px; background-color: brown;">
+            <h1>Contact Us</h1>
+        </div>
+
+    </body>
+</html>
